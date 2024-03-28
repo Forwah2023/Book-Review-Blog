2d471fe5e67a4b0ce0bcd8a06afcca8f408c1211
Forwah Amstrong <lmsoftware2023@gmail.com>
2024-03-28 13:15:53 +0100

    Created custom user and PostgreSql
    - Added a custom user model using BaseUser.
    -Changed default database to PostgreSql.
    -Added tests for the customUser model.

---
+++ users/tests.py
@@ -0,0 +1,29 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+class CustomUserTests(TestCase):
+    def test_create_user(self):
+        User = get_user_model()
+        user = User.objects.create_user(
+        username='special',
+        email='special@gmail.com',
+        password='testpass123'
+        )
+        self.assertEqual(user.username, 'special')
+        self.assertEqual(user.email, 'special@gmail.com')
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
+    
+    def test_create_superuser(self):
+        User = get_user_model()
+        admin_user = User.objects.create_superuser(
+        username='assistantadmin',
+        email='assistantadmin@gmail.com',
+        password='testpass123'
+        )
+        self.assertEqual(admin_user.username, 'assistantadmin')
+        self.assertEqual(admin_user.email, 'assistantadmin@gmail.com')
+        self.assertTrue(admin_user.is_active)
+        self.assertTrue(admin_user.is_staff)
+        self.assertTrue(admin_user.is_superuser)
