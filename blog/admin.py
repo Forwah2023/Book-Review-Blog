54c89c57c166a4aa2c4e05c983f70b875c1dea65
Forwah Amstrong <lmsoftware2023@gmail.com>
2024-05-09 08:29:54 +0100

    Update admin.py
    Included fields to display for the Post model

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Post,Subscriber,Bookmark
+
+class PostAdmin(admin.ModelAdmin):
+	list_display = ("title","author","rating",)
+
+admin.site.register(Post,PostAdmin)
+admin.site.register(Subscriber)
+admin.site.register(Bookmark)
