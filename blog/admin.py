498b1732e33b99988ca110be9667770182ce599b
Forwah Amstrong <lmsoftware2023@gmail.com>
2024-05-09 14:13:24 +0100

    display bookmarks under Post in admin
    Added inline display for Post in admin

--- blog/admin.py
+++ blog/admin.py
@@ -3,7 +3,13 @@ from django.contrib import admin
 # Register your models here.
 from .models import Post,Subscriber,Bookmark
 
+class BookmarkInline(admin.TabularInline):
+	model = Bookmark
+
 class PostAdmin(admin.ModelAdmin):
+	inlines = [
+		BookmarkInline,
+		]
 	list_display = ("title","author","rating",)
 
 admin.site.register(Post,PostAdmin)
