147aaddbc92a3c204df6582fbe89750004988235
Forwah Amstrong <lmsoftware2023@gmail.com>
2024-05-10 15:44:49 +0100

    further XSS protection settings

--- book_review_project/urls.py
+++ book_review_project/urls.py
@@ -20,7 +20,7 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin90/', admin.site.urls),
     #authentification
     path('accounts/', include('allauth.urls')), 
     #local
