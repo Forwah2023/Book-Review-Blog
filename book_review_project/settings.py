e9436cbc59c5e6ef4ae28fbafe4e7dd63f346600
Forwah Amstrong <lmsoftware2023@gmail.com>
2024-05-12 08:11:24 +0100

    added domain name of Heroku app

--- book_review_project/settings.py
+++ book_review_project/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['intense-beach-72172.herokuapp.com', 'localhost', '127.0.0.1']
 # django-debug-toolbar
 INTERNAL_IPS = ['127.0.0.1']
 
